4c28ae7ce6f8459e5c9a3e7c60f4e5f6c421203e
adilafsar99 <adilafsar99@gmail.com>
2026-03-17 20:03:14 +0500

    Show the proper error message using the constraint validation API.

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ const handleInput = (input) => {
     const inputParent = input.parentElement;
     const errorBox = inputParent.lastElementChild;
     if (!input.checkValidity()) {
+        if (input.validity.valueMissing) {
+            errorBox.textContent = "Required field!";
+        }
+        if (input.validity.typeMismatch) {
+            errorBox.textContent = 'Required format: name@example.com';
+        }
+        if (input.validity.patternMismatch) {
+            errorBox.textContent = 'Required format: 03xxxxxxxxx';
+        }
+        if (input.validity.tooShort) {
+            errorBox.textContent = 'Minimum length: 8 characters.'
+        }
         inputParent.classList.add('invalid');
         errorBox.classList.add('visible');
-        console.log(input.name)
     }
     else {
         inputParent.classList.remove('invalid');
@@ -13,10 +24,10 @@ const handleInput = (input) => {
 }
 
 const handleSubmit = (event) => {
-     event.preventDefault();
-     for (const input of inputs) {
+    event.preventDefault();
+    for (const input of inputs) {
         handleInput(input);
-     }
+    }
 }
 
 const signupForm = document.querySelector('.signup-form');
